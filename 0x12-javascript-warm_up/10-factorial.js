ba554f352c3516e55d7bb618f6d72e18eab2385c
mostafa-araheim <mostafa.araheim@gmail.com>
2024-06-20 14:47:58 +0300

    10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+function factorial (number) {
+  if (isNaN(number) || number === 0) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
+}
+const { argv } = require('process');
+
+const n = parseInt(argv[2]);
+
+console.log(factorial(n));
