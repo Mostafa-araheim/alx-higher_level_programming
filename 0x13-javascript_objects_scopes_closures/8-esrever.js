cf61975c44c16f8af3522aefa78f30ffe244917e
mostafa-araheim <mostafa.araheim@gmail.com>
2024-06-20 19:25:01 +0300

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const j = 0;
+  let j = 0;
   const arr = [];
-  for (let i = list.length - 1; i >= 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--, j++) {
     arr[j] = list[i];
   }
   return arr;
