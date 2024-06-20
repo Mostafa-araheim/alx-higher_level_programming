daa0a1f91c92a1328a99a599573f8c9e8778c098
mostafa-araheim <mostafa.araheim@gmail.com>
2024-06-20 17:34:20 +0300

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,5 +8,15 @@ class Rectangle {
     this.width = w;
     this.height = h;
   }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rect = '';
+      for (let i = 0; i < this.width; i++) {
+        rect += 'X';
+      }
+      console.log(rect);
+    }
+  }
 }
 module.exports = Rectangle;
