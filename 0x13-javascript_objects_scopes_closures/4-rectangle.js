bcbd15e9c97dd93f4036b09f0534da31b7751701
mostafa-araheim <mostafa.araheim@gmail.com>
2024-06-20 17:55:37 +0300

    4-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+class Rectangle {
+  constructor (w, h) {
+    if ((isNaN(w) || isNaN(h)) || w <= 0 || h <= 0) {
+      return 'Rectangle {}';
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let rect = '';
+      for (let i = 0; i < this.width; i++) {
+        rect += 'X';
+      }
+      console.log(rect);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = Rectangle;
